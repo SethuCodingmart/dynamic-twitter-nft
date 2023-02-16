be9b9dbe25647cf4f13e47387d065e02c53b2a71
venkatasai510 <venkatasai@sketchbrahma.com>
2023-02-16 14:56:55 +0530

    installed slick slider

--- src/components/modal/badgesModal/index.jsx
+++ src/components/modal/badgesModal/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Slider from "react-slick";
+
 import GoldTrophy from "../../../assets/images/badges/goldTropy.svg";
 import Close from "../../../assets/images/modal/close.svg";
 
+
 import "./badges.scss";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
 
 const BadgeModal = ({closeBadges}) => {
+  const settings = {
+    dots: false,
+    arrows : false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
+
+
+
   return (
     <section className="badgesModal">
       <div className="badgesModalContainer">
@@ -13,30 +30,56 @@ const BadgeModal = ({closeBadges}) => {
             <a>SHARE</a>
         </div>
         <div className="badgesModalContent">
-          <div className="badgesModalCard">
-            <img src={GoldTrophy} alt="GoldTropy" />
-            <div>
-              <h3>Gold Cup</h3>
-              <p>Won first place in a verified forum competition</p>
+          <Slider {...settings}>
+            <div className="badgesModalCard">
+              <img src={GoldTrophy} alt="GoldTropy" />
+              <div>
+                <h3>Gold Cup</h3>
+                <p>Won first place in a verified forum competition</p>
+              </div>
+              <label>Unlocked on 1st January 2022</label>
             </div>
-            <label>Unlocked on 1st January 2022</label>
-          </div>
-          <div className="badgesModalCard">
-            <img src={GoldTrophy} alt="GoldTropy" />
-            <div>
-              <h3>Gold Cup</h3>
-              <p>Won first place in a verified forum competition</p>
+            <div className="badgesModalCard">
+              <img src={GoldTrophy} alt="GoldTropy" />
+              <div>
+                <h3>Gold Cup</h3>
+                <p>Won first place in a verified forum competition</p>
+              </div>
+              <label>Unlocked on 1st January 2022</label>
             </div>
-            <label>Unlocked on 1st January 2022</label>
-          </div>
-          <div className="badgesModalCard">
-            <img src={GoldTrophy} alt="GoldTropy" />
-            <div>
-              <h3>Gold Cup</h3>
-              <p>Won first place in a verified forum competition</p>
+            <div className="badgesModalCard">
+              <img src={GoldTrophy} alt="GoldTropy" />
+              <div>
+                <h3>Gold Cup</h3>
+                <p>Won first place in a verified forum competition</p>
+              </div>
+              <label>Unlocked on 1st January 2022</label>
             </div>
-            <label>Unlocked on 1st January 2022</label>
-          </div>
+            <div className="badgesModalCard">
+              <img src={GoldTrophy} alt="GoldTropy" />
+              <div>
+                <h3>Gold Cup</h3>
+                <p>Won first place in a verified forum competition</p>
+              </div>
+              <label>Unlocked on 1st January 2022</label>
+            </div>
+            <div className="badgesModalCard">
+              <img src={GoldTrophy} alt="GoldTropy" />
+              <div>
+                <h3>Gold Cup</h3>
+                <p>Won first place in a verified forum competition</p>
+              </div>
+              <label>Unlocked on 1st January 2022</label>
+            </div>
+            <div className="badgesModalCard">
+              <img src={GoldTrophy} alt="GoldTropy" />
+              <div>
+                <h3>Gold Cup</h3>
+                <p>Won first place in a verified forum competition</p>
+              </div>
+              <label>Unlocked on 1st January 2022</label>
+            </div>
+          </Slider>
         </div>
         <div className="badgesModalClose">
             <img src={Close} alt="close" onClick={() => closeBadges()}/>
